94621770f52d32e8948dc412bc63828a907dd21f
pandaninjas <101084582+pandaninjas@users.noreply.github.com>
2022-10-18 08:39:46 -0700

    Minor change given that the name has been changed

--- placeholdermc-alert.js
+++
@@ -1,6 +0,0 @@
-window.onload = function () {
-    let elem = document.createElement("div");
-    elem.style.cssText = "background-color: coral;font: 15px Arial, sans-serif;padding: 10px;position: relative;z-index:auto;margin:0px 0px 0px 0px;";
-    elem.innerHTML = "<p>PolyMC has been taken over by a hostile maintainer who may have been hacked. Please see <a href='https://gist.github.com/Earthcomputer/dc65391f84a2c19ebac6c33506fd7751'>this</a> for info on how to stay safe if you're a user. The new GitHub is PlaceholderMC. Want to spread the word? See <a href='https://github.com/thefightagainstmalware/PolyMCAlert'>this</a> link</p>"
-    document.body.insertBefore(elem, document.body.children[0]);
-}
